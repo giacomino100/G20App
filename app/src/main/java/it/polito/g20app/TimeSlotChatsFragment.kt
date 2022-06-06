3cc3a5ab7e87855215360937640c21fd3f742d53
pietroubertini <s292522@studenti.polito.it>
2022-06-06 19:16:24 +0200

    Deleted OnViewCreated

--- app/src/main/java/it/polito/g20app/TimeSlotChatsFragment.kt
+++ app/src/main/java/it/polito/g20app/TimeSlotChatsFragment.kt
@@ -1,16 +1,11 @@
 package it.polito.g20app
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
@@ -30,31 +25,9 @@ class TimeSlotChatsFragment : Fragment(R.layout.fragment_timeslot_chats) {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_timeslot_chats, container, false)
-        Log.d("navigation", "TimeSlotChatsFragment")
-
-        /*
-        requireActivity()
-            .onBackPressedDispatcher
-            .addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    val bundle = Bundle()
-                    bundle.putString("id",idTimeSlot)
-
-                    if (isEnabled) {
-                        findNavController().navigate(R.id.action_nav_timeslot_chats_fragment_to_nav_slot_details,bundle)
-                        Log.d("back",idTimeSlot)
-                        isEnabled = false
-                    }
-                }
-            }
-            )*/
-        return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val rv = view.findViewById<RecyclerView>(R.id.rv_chats)
+        val rv = root.findViewById<RecyclerView>(R.id.rv_chats)
         (activity as FirebaseActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        rv.layoutManager = LinearLayoutManager(view.context)
+        rv.layoutManager = LinearLayoutManager(root.context)
 
         arguments.let {
             idTimeSlot = it!!.getString("idTimeSlot") as String
@@ -62,12 +35,11 @@ class TimeSlotChatsFragment : Fragment(R.layout.fragment_timeslot_chats) {
 
         viewModelC.chats.observe(viewLifecycleOwner) { it ->
             if (it.none { c -> c.idTimeSlot == arguments.let { b -> b!!.getString("idTimeSlot") } })
-                Snackbar.make(view, "No opened chats for the selected timeslot", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(root, "No opened chats for the selected timeslot", Snackbar.LENGTH_LONG).show()
             val adapter = ChatAdapter(it.filter { arguments.let { b -> b!!.get("idTimeSlot")
             } == it.idTimeSlot && it.idVendor == auth.uid} as MutableList<Chat>)
             rv.adapter = adapter
         }
-        super.onViewCreated(view, savedInstanceState)
+        return root
     }
-
 }
