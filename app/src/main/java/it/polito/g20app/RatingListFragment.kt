69b1123335c07f95cc6b9571c48a46d7bd420789
MammoDde <mammo3dade@gmail.com>
2022-06-07 22:34:07 +0200

    fixed filters for list ratings

--- app/src/main/java/it/polito/g20app/RatingListFragment.kt
+++ app/src/main/java/it/polito/g20app/RatingListFragment.kt
@@ -36,8 +36,7 @@ class RatingListFragment : Fragment(R.layout.fragment_list_rating) {
 
             val ratings = it.filter{
                 it1 ->
-                it1.idVendor == auth.uid
-                it1.idWriter != auth.uid
+                it1.idVendor == auth.uid && it1.idWriter != auth.uid
 
             }
             if(ratings.isEmpty()){
@@ -57,8 +56,7 @@ class RatingListFragment : Fragment(R.layout.fragment_list_rating) {
             val emptyListMsg = root.findViewById<TextView>(R.id.emptyBuyerReviews)
             val ratings = it.filter{
                 it1 ->
-                it1.idBuyer == auth.uid
-                it1.idWriter != auth.uid
+                it1.idBuyer == auth.uid && it1.idWriter != auth.uid
             }
             Log.d("ratingB",ratings.toString())
             if(ratings.isEmpty()){
