fcba3cd38cd2f6ae2b8a6a4d4b309f26327e9a38
Giacomo <giacomo.inghilleri@gmail.com>
2022-06-07 19:45:50 +0200

    adding rating

--- app/src/main/java/it/polito/g20app/RatingFragment.kt
+++ app/src/main/java/it/polito/g20app/RatingFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.RatingBar
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import com.google.android.material.snackbar.Snackbar
@@ -39,11 +40,10 @@ class RatingFragment : Fragment() {
             .onBackPressedDispatcher
             .addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    val rate = root.findViewById<EditText>(R.id.ratingBarAssign).text.toString()
-                    val comment = root.findViewById<EditText>(R.id.comment_rating).text.toString()
-
-                    val docData = Rating("", idVendor, idBuyer, idTimeSlot, rate, comment)
-
+                    val rate = root.findViewById<RatingBar>(R.id.ratingBarAssign).numStars
+                    val comment = root.findViewById<EditText>(R.id.editTextRatingComment).text.toString()
+                    val docData = Rating("", idVendor, idBuyer, idTimeSlot, rate.toString(), comment)
+                    Log.d("ratings", docData.toString())
                     viewModelR.addRating(docData)
                     if (isEnabled) {
                         isEnabled = false
